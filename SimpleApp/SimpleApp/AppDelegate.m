af0dfcdea2222a75c1db5d69f95e36e52a3d733c
wangyuqing <wangyuqing.rachel@bytedance.com>
2021-04-06 13:26:14 +0800

    set window rootViewController as a Tabbar controller

--- SimpleApp/SimpleApp/AppDelegate.m
+++ SimpleApp/SimpleApp/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,15 +18,22 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    
     //create a tabbar controller
     UITabBarController *tabbarController  = [[UITabBarController alloc] init];
     
+    
+    //a navigation controller needs a root view
+    ViewController *viewController = [[ViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    
     //create 4 view controller and set their background colors.
-    UIViewController *viewController1 = [[UIViewController alloc]init];
-    viewController1.view.backgroundColor = [UIColor systemRedColor];
-    viewController1.tabBarItem.title = @"news";
-    viewController1.tabBarItem.image = [UIImage imageNamed:@"file path1"];
-    viewController1.tabBarItem.selectedImage = [UIImage imageNamed:@"file path2"];
+    
+//    UIViewController *viewController1 = [[UIViewController alloc]init];
+    navigationController.view.backgroundColor = [UIColor systemRedColor]; //set news page as navigation controller
+    navigationController.tabBarItem.title = @"news";
+    navigationController.tabBarItem.image = [UIImage imageNamed:@"file path1"];
+    navigationController.tabBarItem.selectedImage = [UIImage imageNamed:@"file path2"];
     
     
     UIViewController *viewController2 = [[UIViewController alloc]init];
@@ -47,11 +55,14 @@
     viewController4.tabBarItem.selectedImage = [UIImage imageNamed:@"file path8"];
     
     //add 4 view controllers to tabbar controller
-    [tabbarController setViewControllers:@[viewController1, viewController2, viewController3, viewController4]];
+    [tabbarController setViewControllers:@[navigationController, viewController2, viewController3, viewController4]];
     
     
     
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = tabbarController; //set tabbarController as default (no longer a single view)
+    [self.window makeKeyAndVisible];
+    
     
     
     
--- SimpleApp/SimpleApp/ViewController.m
+++ SimpleApp/SimpleApp/ViewController.m
@@ -16,11 +16,30 @@
 
 - (void)viewDidLoad {
     
+    self.view.backgroundColor = [UIColor whiteColor]; //set view controller color
+    
     UIView *myView = [[UIView alloc] init];
-    myView.backgroundColor = [UIColor redColor];
-    myView.frame = CGRectMake(100, 100, 100, 100);
+    myView.backgroundColor = [UIColor systemTealColor];
+    myView.frame = CGRectMake(148, 150, 100, 50);
     [self.view addSubview:myView];
+    
+    self.navigationItem.title = @"NEWS";
+    
+    //create gesture
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushController)];
+    //add gesture to myView(a teal view)
+    [myView addGestureRecognizer:tapGesture];
 
+}
+- (void)pushController{
+    UIViewController *viewController = [[UIViewController alloc] init];
+    viewController.navigationItem.title = @"CONTENT";
+    
+    //add right button
+    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"RIGHT" style:UIBarButtonItemStylePlain target:self action:nil];
+    
+    
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 
