2c9aeec1c179ee502f4c0221390d0e27be576d64
wangyuqing <wangyuqing.rachel@bytedance.com>
2021-04-07 13:20:25 +0800

    some changes

--- SimpleApp/SimpleApp/NewsTableViewCell.m
+++ SimpleApp/SimpleApp/NewsTableViewCell.m
@@ -19,48 +19,48 @@
 
 @implementation NewsTableViewCell
 
+//set our own cell style
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(nullable NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if(self){
-        //BLOCK???
-        [self.contentView addSubview:({
-            self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 15, 300, 50)];
-            self.titleLabel.backgroundColor = [UIColor whiteColor];
-            self.titleLabel.font = [UIFont systemFontOfSize:16];
-            self.titleLabel.textColor = [UIColor blackColor];
-            self.titleLabel;
-        })];
-        [self.contentView addSubview:({
-            self.sourceLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 50, 20)];
-            self.sourceLabel.backgroundColor = [UIColor whiteColor];
-            self.sourceLabel.font = [UIFont systemFontOfSize:12];
-            self.sourceLabel.textColor = [UIColor grayColor];
-            self.sourceLabel;
-        })];
-        [self.contentView addSubview:({
-            self.commentLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 80, 50, 20)];
-            self.commentLabel.backgroundColor = [UIColor whiteColor];
-            self.commentLabel.font = [UIFont systemFontOfSize:12];
-            self.commentLabel.textColor = [UIColor grayColor];
-            self.commentLabel;
-        })];
-        [self.contentView addSubview:({
-            self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(200, 80, 50, 20)];
-            self.timeLabel.backgroundColor = [UIColor whiteColor];
-            self.timeLabel.font = [UIFont systemFontOfSize:12];
-            self.timeLabel.textColor = [UIColor grayColor];
-            self.timeLabel;
-        })];
+        //
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 15, 300, 50)];
+        self.titleLabel.backgroundColor = [UIColor whiteColor];
+        self.titleLabel.font = [UIFont systemFontOfSize:16];
+        self.titleLabel.textColor = [UIColor blackColor];
+        [self.contentView addSubview:self.titleLabel];
+        
+        
+        self.sourceLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 50, 20)];
+        self.sourceLabel.backgroundColor = [UIColor whiteColor];
+        self.sourceLabel.font = [UIFont systemFontOfSize:12];
+        self.sourceLabel.textColor = [UIColor grayColor];
+        [self.contentView addSubview: self.sourceLabel];
+        
+        
+        self.commentLabel = [[UILabel alloc] initWithFrame:CGRectMake(627, 80, 50, 20)]; //627 will be reset
+        self.commentLabel.backgroundColor = [UIColor whiteColor];
+        self.commentLabel.font = [UIFont systemFontOfSize:12];
+        self.commentLabel.textColor = [UIColor grayColor];
+        [self.contentView addSubview:self.commentLabel];
+        
+        
+        self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(627, 80, 50, 20)]; //627 will be reset
+        self.timeLabel.backgroundColor = [UIColor whiteColor];
+        self.timeLabel.font = [UIFont systemFontOfSize:12];
+        self.timeLabel.textColor = [UIColor grayColor];
+        [self.contentView addSubview:self.timeLabel];
+        
     }
     return self;
 }
 
 
 - (void) layoutTableViewCell{
-    self.titleLabel.text = @"And if you solve enough problems, you get to come home.";
+    self.titleLabel.text = @"Covid symptoms: Is it a cold, flu or coronavirus?";
     self.titleLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
     
-    self.sourceLabel.text = @"BBC";
+    self.sourceLabel.text = @"BBC News";
     [self.sourceLabel sizeToFit];
     
     self.commentLabel.text = @"627 comments";
